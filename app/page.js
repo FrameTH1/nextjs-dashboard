b6323e16adf19c4881d923871c647633403c7084
Frame <sukitpromputta.work@gmail.com>
2024-04-26 15:16:04 +0700

    uploaded

---
+++ app/page.js
@@ -0,0 +1,12 @@
+
+import MyChart from "@/components/MyChart"
+import MyChartSum from "@/components/MyChartSum"
+
+export default function Page() {
+  return (
+    <div>
+      <MyChart/>
+      <MyChartSum/>
+    </div>
+  )
+}
---
+++ utils/db.js
@@ -0,0 +1,7 @@
+const mysql = require('mysql2')
+export const mysqlPool = mysql.createPool({
+    host : "43.229.148.5",
+    user : "Frame",
+    password : "Sukitxyz1@",
+    database : "6500421_dit205"
+})
